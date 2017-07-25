2594939b323242bcdf37409ba087ddfe4e29bcac
mnowik <nowicki.matthieu@gmail.com>
2017-07-25 23:47:10 +0300

    basic app and publish it with ngrok

---
+++ initialize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+old_pwd=$(pwd)
+cd ~/
+if [[ -e y2-venv ]]; then
+    source ~/y2-venv/bin/activate
+else
+     sudo apt-get install virtualenv
+     virtualenv -p $(which python3) y2-venv
+     source ~/y2-venv/bin/activate
+     pip install -r $old_pwd/requirements.txt
+fi
+cd $old_pwd
+
+sudo apt-get install ngrok-client
+export FLASK_APP=app.py
+export FLASK_DEBUG=1
+
